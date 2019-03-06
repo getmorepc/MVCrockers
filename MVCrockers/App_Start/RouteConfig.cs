5b0e6ef88756ab1f0a9771620ae077db8e178db4
Daniel Buchanan <daniel@getmorepc.com>
2019-03-06 16:44:07 -0500

    Lots of Learning
    Done

--- MVCrockers/App_Start/RouteConfig.cs
+++ MVCrockers/App_Start/RouteConfig.cs
@@ -15,9 +15,17 @@ namespace MVCrockers
 
             routes.MapRoute(
                 name: "Default",
+                url: "backstage/{secret}/{format}",
+                defaults: new { controller = "Home", action = "Backstage", format = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "Backstage",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+
         }
     }
 }
--- MVCrockers/Controllers/HomeController.cs
+++ MVCrockers/Controllers/HomeController.cs
@@ -28,9 +28,36 @@ namespace MVCrockers.Controllers
             return View("About");
         }
 
+
+
+
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "What do you think?";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Contact(string message)
+        {
+            // TODO: Save this and act on it
+            ViewBag.Message = "Thanks for the feedback!";
+            return View();
+        }
+
+        public ActionResult Backstage(string secret, string format)
+        {
+            if (secret != "special")
+                return new HttpStatusCodeResult(403);
+
+            if (format == "text")
+                return Content("You Rock!");
+            else if (format == "json")
+                return Json(new { password = "You rock!", expires = DateTime.UtcNow.ToShortDateString() },
+                    JsonRequestBehavior.AllowGet);
+            else if (format == "clean")
+                return PartialView();
 
             return View();
         }

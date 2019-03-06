ebca8a5bd870c7b2e819fbea2c0c512601fd6265
Daniel Buchanan <daniel@getmorepc.com>
2019-03-06 15:40:50 -0500

    Add project files.

---
+++ MVCrockers/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCrockers.Startup))]
+namespace MVCrockers
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
